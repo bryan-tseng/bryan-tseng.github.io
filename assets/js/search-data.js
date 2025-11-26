1039eae0693bf742f73a4e1ef158aa4542f0c97c
bryan-tseng <bryan930180@gmail.com>
2025-11-26 05:14:49 +0000

    Deploying to gh-pages from @ bryan-tseng/bryan-tseng.github.io@cf97cbb7b4dc1f106096edc0b5294cafd05f8d09 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "The most updated info is in the pdf file.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
